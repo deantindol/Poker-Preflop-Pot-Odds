872d95c81c13b2292b1babf42bcc05ac1d8eda5d
deantindol <161906549+deantindol@users.noreply.github.com>
2024-03-12 12:23:21 -0400

    poker driver now can take table input with -t arg

--- src/poker/PokerDriver.java
+++ src/poker/PokerDriver.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 /**
  * Accepts user input for 4 cards, two for each player.
- * Resulting pre-flop odds are compyted and printed.
+ * Resulting pre-flop odds are computed and printed.
  */
 public class PokerDriver extends HandOperations {
 
     public static void main(String[] args) {
+        boolean longOutput = false;
+        Card[] tab = new Card[4];
         Scanner s = new Scanner(System.in);
         System.out.println("Use the format '10 of Clubs' or 'Ace of Spades'");
 
@@ -28,16 +30,82 @@ public class PokerDriver extends HandOperations {
         System.out.print("Enter player 2 Card 2: ");
         String inputFour = s.nextLine();
         Card four = new Card(inputFour);
+        
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                if (args[i].equals("-l")) {
+                    longOutput = true;
+                } else if (args[i].equals("-t") && args.length > i){
+                    if (args[i + 1].equals("3")) {
+                        System.out.print("Enter flop card 1: ");
+                        String inputFive = s.nextLine();
+                        Card five = new Card(inputFive);
+                        
+                        System.out.print("Enter flop card 2: ");
+                        String inputSix = s.nextLine();
+                        Card six = new Card(inputSix);
+                        
+                        System.out.print("Enter flop card 3: ");
+                        String inputSeven = s.nextLine();
+                        Card seven = new Card(inputSeven);
+                        
+                        Card[] build = {five, six, seven};
+                        tab = build;
+                        
+                        System.out.print("Table : ");
+                        for (int j = 0; j < tab.length; j++) {
+                            System.out.print(tab[j]);
+                            if (j != tab.length - 1) {
+                                System.out.print(", ");
+                            }
+                        }
+                        System.out.println();
+                    }
+                } else if (args[i + 1].equals("4")) {
+                    System.out.print("Enter flop card 1: ");
+                    String inputFive = s.nextLine();
+                    Card five = new Card(inputFive);
+                    
+                    System.out.print("Enter flop card 2: ");
+                    String inputSix = s.nextLine();
+                    Card six = new Card(inputSix);
+                
+                    System.out.print("Enter flop card 3: ");
+                    String inputSeven = s.nextLine();
+                    Card seven = new Card(inputSeven);
+                        
+                    System.out.print("Enter turn card: ");
+                    String inputEight = s.nextLine();
+                    Card eight = new Card(inputEight);
+                        
+                    tab[0] = five;
+                    tab[1] = six;
+                    tab[2] = seven;
+                    tab[3] = eight;
+                        
+                    System.out.print("Table : ");
+                    for (int j = 0; j < tab.length; j++) {
+                        System.out.print(tab[j]);
+                        if (j != tab.length - 1) {
+                            System.out.print(", ");
+                        }
+                    }
+                    System.out.println();
+                }
+            }
+            
+        }
 
         String oneFullName = one.toString();
         String twoFullName = two.toString();
         String threeFullName = three.toString();
         String fourFullName = four.toString();
+        
 
         if (oneFullName.equals(twoFullName) || threeFullName.equals(oneFullName) ||
             threeFullName.equals(twoFullName) || fourFullName.equals(oneFullName) ||
             fourFullName.equals(threeFullName) || fourFullName.equals(twoFullName)) {
-        	s.close();
+            s.close();
             throw new IllegalArgumentException("No card can be the same as another");
         }
         
@@ -54,12 +122,11 @@ public class PokerDriver extends HandOperations {
             playerTwoHand[0] = four;
             playerTwoHand[1] = three;
         }
-        Card[] tab = new Card[0];
-        if (args.length > 0 && args[0].equalsIgnoreCase("-l")) {
-            onevone(playerOneHand, playerTwoHand, true, tab);
-        } else {
-            onevone(playerOneHand, playerTwoHand, false, tab);
+        
+        if (tab[0] == null) {
+            tab = new Card[0];
         }
+        onevone(playerOneHand, playerTwoHand, longOutput, tab);
 
         s.close();
         System.out.println("Executed in: " + (System.currentTimeMillis() - startTime) + " ms");
